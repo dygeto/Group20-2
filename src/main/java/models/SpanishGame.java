31d70b4dd25cc94b6730496214a332744a8bc5d7
dygeto <tomlinsd@oregonstate.edu>
2016-02-11 18:32:06 -0800

    asdfd

--- src/main/java/models/SpanishGame.java
+++ src/main/java/models/SpanishGame.java
@@ -12,7 +12,7 @@ public class SpanishGame extends Game {
     public SpanishGame();
 
     public void buildDeck() {
-        for(int i = 1; i < 13; i++){
+        for(int i = 1; i < 11; i++){
             deck.add(new Card(i,Suit.Coins));
             deck.add(new Card(i,Suit.Cups));
             deck.add(new Card(i,Suit.Swords));
