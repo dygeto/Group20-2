58bfcf1bd9b83cd29caa240b5144b7caca2cdb8c
dominic <giacoppd@oregonstate.edu>
2016-02-11 18:44:41 -0800

    Fixed tests for spanish deck

--- src/test/java/models/testGame.java
+++ src/test/java/models/testGame.java
@@ -36,12 +36,27 @@ public class testGame {
 //        assertEquals(51,g.deck.size());
     }
 
+    @Test
+    public void testSpanishDeckCount() {
+        Game s = new SpanishGame();
+        assertEquals(40,s.deck.size());
+        s.remove(2);
+        assertEquals(39,g.deck.size());
+    }
+
 
     @Test
     public void testGameBuildDeck(){
         Game g = new Game();
         g.buildDeck();
         assertEquals(52,g.deck.size());
+    }
+
+    @Test
+    public void testGameBuildSpanishDeck(){
+        Game g = new SpanishGame();
+        g.buildDeck();
+        assertEquals(40,g.deck.size());
     }
 
     @Test
@@ -53,8 +68,28 @@ public class testGame {
     }
 
     @Test
+    public void testSpanishGameInit(){
+        Game g = new SpanishGame();
+        g.buildDeck();
+        g.shuffle();
+        assertNotEquals(0,g.deck.get(0).getValue());
+    }
+
+    @Test
     public void testGameStart(){
         Game g = new Game();
+        g.buildDeck();
+        g.shuffle();
+        g.dealFour();
+        assertEquals(1,g.cols.get(0).size());
+        assertEquals(1,g.cols.get(1).size());
+        assertEquals(1,g.cols.get(2).size());
+        assertEquals(1,g.cols.get(3).size());
+    }
+
+    @Test
+    public void testSpanishGameStart(){
+        Game g = new SpanishGame();
         g.buildDeck();
         g.shuffle();
         g.dealFour();
@@ -76,8 +111,28 @@ public class testGame {
     }
 
     @Test
+    public void testSpanishCustomDeal(){
+        Game g = new SpanishGame();
+        g.buildDeck();
+        g.customDeal(0,3,6,9);
+        assertEquals("2Clubs",g.cols.get(0).get(0).toString());
+        assertEquals("3Clubs",g.cols.get(1).get(0).toString());
+        assertEquals("4Clubs",g.cols.get(2).get(0).toString());
+        assertEquals("5Clubs",g.cols.get(3).get(0).toString());
+    }
+
+    @Test
     public void testRemoveFunction(){
         Game g = new Game();
+        g.buildDeck();
+        g.customDeal(0,3,6,9);
+        g.remove(2);
+        assertEquals(0,g.cols.get(2).size());
+    }
+
+    @Test
+    public void testSpanishRemoveFunction(){
+        Game g = new SpanishGame();
         g.buildDeck();
         g.customDeal(0,3,6,9);
         g.remove(2);
